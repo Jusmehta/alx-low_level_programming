cc08941390f714b53a090872ed58b7355b7797ca
Justin <Jusmehta31@gmail.com>
2022-09-08 15:53:16 -0700

    final

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'and that piece of art is useful' - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int void(main)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

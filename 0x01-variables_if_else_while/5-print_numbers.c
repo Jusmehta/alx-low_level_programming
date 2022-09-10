ca663296dd3b71400de95ff9337fa69d258dbfe3
Justin <Jusmehta31@gmail.com>
2022-09-09 18:49:22 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program prints single digit numbers of base to from 0
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		printf("%i", digit);
+	putchar("\n");
+
+	return (0);
+}

0762a17dec4f25531022ea63d57d482c969879c3
Justin <Jusmehta31@gmail.com>
2022-09-15 11:34:40 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints fibbonaci series terms starting with 1 and 2
+ * separated by a comma plus a space
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	for (i = 0; i < 50; i++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}

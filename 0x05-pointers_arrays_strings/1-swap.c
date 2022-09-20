bb748a1cba3c7e1faa7c527bf076919bb7ec383c
Justin <Jusmehta31@gmail.com>
2022-09-19 18:32:46 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * swap_int - swaps two integer values
+ * swap_int - swaps two integers' values
  * @a: first integer
  * @b: second integer
- * Return: nothing
+ * Return: returns nothing
  */
  void swap_int(int *a, int *b)
 {
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,8 @@
  * int prototypes
  */
 
+#include <stdlib.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -17,4 +19,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
+
 #endif /*MAIN_H*/

5461dbaf6b15a2e39f088b674c03f667e208cf94
Justin <Jusmehta31@gmail.com>
2022-09-19 20:01:12 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints somearray elements
+ * @a: array
+ * @n: number of elements
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int leo;
+
+	for (leo = 0; leo < n; leo++)
+	{
+		if (leo != n -1)
+			printf("%d", a[leo]);
+		else
+			printf("%d", a[leo]);
+	}
+	putchar(10);
+}

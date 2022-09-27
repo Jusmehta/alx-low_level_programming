d3f874cbf7495a949b9dd5ee0a83e4ea3e6dc6dd
Justin <Jusmehta31@gmail.com>
2022-09-27 11:18:37 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,8 @@
  * _strchr - function locates a character in a string
  * @s: the string to be checked
  * @c: the character to be located in string
- * @Return:a pointer once @c is found, NULL if @c isn't found
+ * @Return: a pointer once @c is found
+ * NULL if @c isn't found
  */
 
 char *_strchr(char *s, char c)

9ea01c5570611460fac76e6f6a890190d08d6f48
Justin <Jusmehta31@gmail.com>
2022-09-27 11:05:21 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,7 +5,7 @@
  * @dest: the destination loc for the memory area
  * @n: the number of bytes to copy
  * @src: the source loc to copy memory
- * Return: a pointer to the @dest 
+ * Return: a pointer to the @dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)

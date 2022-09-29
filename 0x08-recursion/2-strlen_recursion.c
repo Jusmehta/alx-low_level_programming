6c3ee2a9fe3a3bf25f9cb347f2c98722f8e95436
Justin <Jusmehta31@gmail.com>
2022-09-28 18:11:42 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function returns sring length
+ * @s: the string in question
+ * Return: 0
+ */
+
+int _strlen_recursion(chars)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}	

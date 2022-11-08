f2eae8832688ffff819112c7efcc178358a5fbd3
Justin <Jusmehta31@gmail.com>
2022-11-08 10:38:11 -0800

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,6 +10,18 @@
 size_t print_list(const list_t *h)
 {
 	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+	/*
 	const list_t *ptr = 0;
 
 	if (h == NULL)
@@ -35,4 +47,5 @@ size_t print_list(const list_t *h)
 		}
 	}
 	return (count);
+	*/
 }
